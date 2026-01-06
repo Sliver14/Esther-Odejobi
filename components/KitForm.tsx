7d521a31432ac36a911a3dd157cc2c5d4504fc3c
sliver14 <silverchristopher12@gmail.com>
2026-01-07 00:40:36 +0100

    update

--- components/KitForm.tsx
+++ components/KitForm.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import Script from "next/script";
-
 export default function KitForm() {
   return (
-    <div className="w-full max-w-md mx-auto">
-      {/* Kit will inject the form here */}
-      <div id="kit-form-226466bb" />
-
-      <Script
-        src="https://esther-odejobi.kit.com/2228ecf8bb/index.js"
-        data-uid="2228ecf8bb"
-        strategy="afterInteractive"
-      />
-    </div>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script async data-uid="2228ecf8bb" src="https://esther-odejobi.kit.com/2228ecf8bb/index.js"></script>
+        `,
+      }}
+    />
   );
 }
