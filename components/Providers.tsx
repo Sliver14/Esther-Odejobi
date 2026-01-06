03c31d9b21ec448041f4ca604317e40221ab2e6d
sliver14 <silverchristopher12@gmail.com>
2026-01-06 12:33:13 +0100

    update

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,7 +2,6 @@
 
 import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -12,24 +11,23 @@ interface ProvidersProps {
 }
 
 export function Providers({ children }: ProvidersProps) {
-  // Create QueryClient once per component lifecycle
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        {/* Toast notifications */}
-        <Toaster />      {/* UI library toaster */}
-        <Sonner />       {/* Sonner toaster */}
-
-        {/* App children */}
+        <Toaster />
+        <Sonner />
         {children}
-
-        {/* Devtools only in development */}
-        {/* {process.env.NODE_ENV === "development" && (
-          <ReactQueryDevtools initialIsOpen={false} />
-        )} */}
+        {process.env.NODE_ENV === "development" && <Devtools />}
       </TooltipProvider>
     </QueryClientProvider>
   );
+}
+
+function Devtools() {
+  const DevtoolsComponent =
+    require("@tanstack/react-query-devtools").ReactQueryDevtools;
+
+  return <DevtoolsComponent initialIsOpen={false} />;
 }
